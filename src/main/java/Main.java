57d48efc0f2e748982497b90333a094a813d7404
Mykola Shevchuk <shevchukms@users.noreply.github.com>
2017-09-22 12:50:22 +0300

    2 task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         NaturalNumber naturalNumber = new NaturalNumber();
         naturalNumber.soutArraylist(naturalNumber.findNaturalDel(48));
+        System.out.println("------------------------------");
+        naturalNumber.soutArraylist(naturalNumber.findSimpleDel(49));
     }
 }
--- src/main/java/NaturalNumber.java
+++ src/main/java/NaturalNumber.java
@@ -13,10 +13,38 @@ public class NaturalNumber {
 
     }
 
-    public void soutArraylist(ArrayList arrayList){
-        for (Object o:arrayList) {
-            System.out.println(o);;
+    public ArrayList<Integer> findSimpleDel(Integer number) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        for (int i = 1; i < number; i++) {
+            if ((number % i == 0)& (factorial(i)-1)%i==0) // Wilson's theorem
+            {
+                arrayList.add(i);
+            }
+
+        }
+        return arrayList;
+
+    }
+
+    public Integer factorial(Integer number) {
+        Integer n = 1;
+        for (int i = 1; i < number - 1; i++) {
+            n *= i;
+        }
+        return n;
+    }
+
+
+    public void soutArraylist(ArrayList arrayList) {
+        for (Object o : arrayList) {
+            System.out.println(o);
+            ;
         }
     }
+
+    public ArrayList 
+
+
+
 
 }
