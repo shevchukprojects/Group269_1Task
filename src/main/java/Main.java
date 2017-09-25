7adb62eb59dfa6ce0280b7f4eb530c8452dbc6fa
MykolaShevchuk <shevchuk.mykola93@gmail.com>
2017-09-25 22:14:11 +0300

    3 task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
---
+++ src/main/java/Services/Input.java
@@ -0,0 +1,20 @@
+package Services;
+
+import Services.Scanner;
+
+import java.io.IOException;
+
+public class Input {
+    public static Integer input() throws IOException {
+        System.out.println("Input natural number");
+        try {
+            return Integer.valueOf(Scanner.scanner.readLine());
+
+        } catch (NumberFormatException e) {
+
+            System.out.println("You wrote not a natural number");
+            return input();
+        }
+
+    }
+}
