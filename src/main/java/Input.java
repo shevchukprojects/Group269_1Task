c3df19fe3744cbefa147ca0e9069578d0b2f76dd
MykolaShevchuk <shevchuk.mykola93@gmail.com>
2017-09-25 21:58:09 +0300

    3 task

---
+++ src/main/java/Input.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Input {
+    public static Integer input() throws IOException {
+        System.out.println("Input natural number");
+        return Integer.valueOf(Scanner.scanner.readLine());
+    }
+}
